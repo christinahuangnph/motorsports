c7753953bf441fcc7430f134b597edc127d76304
christinahuangnph <christina.huang@vanderbilt.edu>
2019-01-21 15:02:26 -0600

    Added Timer Interrupt

--- APPS_Validation/APPS_Validation.ino
+++ APPS_Validation/APPS_Validation.ino
@@ -1,32 +1,39 @@
 /*  Christina Huang
- *  ATtiny85 APPS Validation
- *  Transmits to Beaglebone via I2C
- */
+    ATtiny85 APPS Validation
+    Transmits to Beaglebone via I2C
+*/
 
- /*  Pin Configurations
-  *  1 - Reset (PB5)                            8 - VCC +
-  *  2 - Analog Input 3 (PB3) APPS input 1      7 - Analog Input 1, SCK (PB2)
-  *  3 - Analog Input 2 (PB4) APPS input 2      6 - PWM, MISO (PB1)
-  *  4 - GND                                    5 - PWM, MOSI, AREF (PB0)
-  *  
-  *  Connect to i2c-0 on Beaglebone: 0x44E0_B000
-  */
-  
-  #include <"TinyWireM.h"> //I2C ATtiny Library
-  #include <avr/interrupt.h> //Interrupt Handler Library
-  #define I2C_SLAVE_ADDR  0x00 //Beaglebone Address - REPLACE
+/*  Pin Configurations
+    1 - Reset (PB5)                            8 - VCC +
+    2 - Analog Input 3 (PB3) APPS input 1      7 - Analog Input 1, SCK (PB2)
+    3 - Analog Input 2 (PB4) APPS input 2      6 - PWM, MISO (PB1)
+    4 - GND                                    5 - PWM, MOSI, AREF (PB0)
 
-  // define input pins
-  int inputHi = 2;
-  int inputLo = 3;
+    Connect to i2c-0 on Beaglebone: 0x44E0_B000
+*/
 
-  // compare values
-  float hi_max = 4.4;
-  float lo_max = 3.6;
-  float hi_min = 1.6;
-  float lo_min = 0.8;
-  
- void setup() {
+#include <"TinyWireM.h"> //I2C ATtiny Library
+#include <avr/interrupt.h> //Interrupt Handler Library
+#include <avr/io.h>
+#include "TimerOne.h"
+
+#define I2C_SLAVE_ADDR  0x00 //Beaglebone Address - REPLACE
+
+// define input pins
+int inputHi = 2;
+int inputLo = 3;
+
+// compare values
+float hi_max = 4.4;
+float lo_max = 3.6;
+float hi_min = 1.6;
+float lo_min = 0.8;
+
+// constant
+float hasError;
+float currTime;
+
+void setup() {
   // declare input pins as inputs and set pullups
   pinMode(inputHi, INPUT);
   digitalWrite(inputHi, HIGH);
@@ -38,41 +45,46 @@
 
   //SET UP BEAGLEBONE COMMUNICATION STUFF
   TinyWireM.beginTransmission(uint8_t I2C_SLAVE_ADDR); //set up slave
-  
 }
+
+
+void timerServiceRoutine() { // timer interrupt service routine
+    if()
+}
+
 
 void loop() {
   // read in pedal values
   float sensorValHi = analogRead(inputHi); //read in raw val
   float sensorValLo = analogRead(inputLo); //read in raw val
-  
+
   // convert to percents
   float percentHi = convertToPercent((sensorValHi - hi_min), (hi_max - hi_min));
   float percentLo = convertToPercent((sensorValLo - lo_min), (lo_max - lo_min));
-  
-  // validate percentages
-  boolean valid = valid(percentHi - percentLo);
-  if(!valid) {
-    //Start timer
-    //enable interrupts
-    
-  }
-  // relay pedal values to Beaglebone 
-  delay(10); //wait 10 ms;
-  
 
+  // validate percentages
+  boolean currValid = valid(percentHi - percentLo);
+  if (!currValid) {
+    currTime = millis();
+    hasError = true;
+    Timer1.initialize(currTime);
+    Timer1.attachInterrupt(timerServiceRoutine);
+  }
+  // relay pedal values to Beaglebone - DO THIS
+  
+  delay(10); //wait 10 ms;
 }
 
 /* Convert raw pedal values to percent of total range
- */
+*/
 float convertToPercent(float val, float compare) {
-  return percent = val/compare;
+  return percent = val / compare;
 }
 
 /* Check whether the pedal value difference is valid
- */
+*/
 boolean valid(float val) {
-  if(val > 0.88 || val < 0.72) { //outside the 10% range
+  if (val > 0.88 || val < 0.72) { //outside the 10% range
     return false;
   } else {
     return true;
