9d87019e56707221d27908bb4ea01ac1c772aace
christinahuangnph <christina.huang@vanderbilt.edu>
2019-01-30 21:29:01 -0600

    Compiles Cleanly

--- APPS_Validation/APPS_Validation.ino
+++ APPS_Validation/APPS_Validation.ino
@@ -4,20 +4,31 @@
 */
 
 /*  Pin Configurations
-    1 - Reset (PB5)                            8 - VCC +
-    2 - Analog Input 3 (PB3) APPS input 1      7 - Analog Input 1, SCK (PB2)
-    3 - Analog Input 2 (PB4) APPS input 2      6 - PWM, MISO (PB1)
-    4 - GND                                    5 - PWM, MOSI, AREF (PB0)
-
-    Connect to i2c-0 on Beaglebone: 0x44E0_B000
+    1 - Reset (PB5) Unused                     8 - VCC +
+    2 - Analog Input 3 (PB3) APPS input 1      7 - Analog Input 1, SCK (PB2) SCL
+    3 - Analog Input 2 (PB4) APPS input 2      6 - PWM, MISO (PB1) Unused
+    4 - GND                                    5 - PWM, MOSI, AREF (PB0) SDA
 */
 
-#include <"TinyWireS.h"> //I2C ATtiny Library
+/* I2C Configurations
+ * 0x03 - Good/Bad 00-Bad, 01-Good
+ * 0x04 - Pedal Value Hi
+ * 0x05 - Pedal Value Lo
+ * 
+ * Connect to i2c-2 on Beaglebone
+ */
+
+#include "./TinyWireS/TinyWireS.h" //I2C ATtiny Library
 #include <avr/interrupt.h> //Interrupt Handler Library
 #include <avr/io.h>
-#include "TimerOne.h"
+//#include "./TimerOne/TimerOne.h"
 
-#define I2C_MASTER_ADDR  0x00 //Beaglebone Address - REPLACE
+
+#define I2C_SLAVE_ADDR  0x03 //Beaglebone Address - REPLACE
+#define I@C_SLAVE_ADDR 0x26   //ATtiny Address
+#define VALID 0x03 //Holds Boolean: Good or bad 
+#define PVH 0x04 //Pedal Value High
+#define PVL 0x05 //Pedal Value Low
 
 // define input pins
 int inputHi = 2;
@@ -41,44 +52,13 @@ void setup() {
   digitalWrite(inputLo, HIGH);
 
   // Set Up I2C
-  TinyWireS.begin();  // initialize I2C library
-
-  //SET UP BEAGLEBONE COMMUNICATION STUFF
-  TinyWireS.beginTransmission(uint8_t I2C_SLAVE_ADDR); //set up slave
-}
-
-void loop() {
-  // read in pedal values
-  float sensorValHi = analogRead(inputHi); //read in raw val
-  float sensorValLo = analogRead(inputLo); //read in raw val
-
-  // convert to percents
-  float percentHi = convertToPercent((sensorValHi - hi_min), (hi_max - hi_min));
-  float percentLo = convertToPercent((sensorValLo - lo_min), (lo_max - lo_min));
-
-  // validate percentages
-  boolean good = valid(percentHi - percentLo);
-  if (!good) {
-      loopCounter++;
-      if(loopCounter == 10) { //it's been false for 90 ms
-        //send error message
-        Serial.print("Bad banana"); //for testing purposes
-      }
-    } else {
-      loopCounter = 0;
-    }
-  }
-  
-  // relay pedal values to Beaglebone - FINISH THIS
-  Serial.print("High: " + sensorValHi + " Low: " + sensorValLo); //for testing purposes
-  
-  delay(9); // wait 9 ms;
+  TinyWireS.begin(I2C_SLAVE_ADDR);  // initialize I2C library
 }
 
 /* Convert raw pedal values to percent of total range
 */
 float convertToPercent(float val, float compare) {
-  return percent = val / compare;
+  return val / compare;
 }
 
 /* Check whether the pedal value difference is valid
@@ -90,4 +70,39 @@ boolean valid(float val) {
     return true;
   }
 }
+
+void loop() {
+  // read in pedal values
+  float sensorValHi = analogRead(inputHi); //read in raw val
+  float sensorValLo = analogRead(inputLo); //read in raw val
+
+  // convert to percents
+  float percentHi = convertToPercent((sensorValHi - hi_min), (hi_max - hi_min));
+  float percentLo = convertToPercent((sensorValLo - lo_min), (lo_max - lo_min));
+
+  // send to Beaglebone
+  //TinyWireS.send(sensorValHi);
+  //TinyWireS.send(sensorValLo);
+
+  // validate percentages
+  boolean good = valid(percentHi - percentLo);
+  if (!good) {
+      loopCounter++;
+      if(loopCounter == 10) { //it's been false for 90 ms
+        //send error message
+        //Serial.print("Bad banana"); //for testing purposes
+        TinyWireS.send("00");
+      }
+    } else {
+      loopCounter = 0;
+      TinyWireS.send("01");
+    }
+  
+  // relay pedal values to Beaglebone - FINISH THIS
+//  Serial.print("High: " + sensorValHi + " Low: " + sensorValLo); //for testing purposes
+  
+  delay(9); // wait 9 ms
+}
+
+
 
