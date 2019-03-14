c8e3324fbb39f0a3710d870f6cbedd72647af49e
christinahuangnph <christina.huang@vanderbilt.edu>
2019-03-13 19:18:36 -0500

    Percent transmission to beaglebone works

--- APPS_Validation/APPS_Validation.ino
+++ APPS_Validation/APPS_Validation.ino
@@ -25,21 +25,22 @@
 byte I2C_OWN_ADDR  = 10;
 
 // define input pins
-int inputHi = 3;
-int inputLo = 4;
+int inputHi = 3; //HI
+int inputLo = 4; //LOW
 int SDA = 0;
 int SCL = 2;
-int LED = 0;
 
 // compare values
-float hi_max = 4.4;
-float lo_max = 3.6;
-float hi_min = 1.6;
-float lo_min = 0.8;
+float hi_max = 9.1; //4.4V
+float hi_min = 3.4; //1.6V
+
+float lo_max = 7.8; //3.6V
+float lo_min = 1.7; //0.8V
 
 // global variables
 boolean good = true;
 int loopCounter = 0;
+int val;
 
 void setup() {
   // declare input pins as inputs and set pullups
@@ -50,49 +51,29 @@ void setup() {
   //declare output pins as outputs and set pullups
   TinyWire.begin(I2C_OWN_ADDR);
   TinyWire.onRequest(I2C_ISR);
-
-  // test
-  pinMode(LED, OUTPUT);
 }
 
 /* Check whether the pedal value difference is valid
 */
 boolean valid(float val) {
-  if (val > 0.88 || val < 0.72) { //outside the 10% range
-    return false;
-  } else {
-    return true;
-  }
+  return (val <= 40 && val >= 12);
 }
 
-void I2C_ISR(byte val) {
+void I2C_ISR() {
+  // read in pedal values
+  float sensorValHi = analogRead(inputHi)/100.0; //read in raw val/255*10
+  //float sensorValLo = analogRead(inputLo); //read in raw val
+
+  // convert to percents
+  sensorValHi = (sensorValHi - hi_min + 20)/77.0*100.0; //between 0 and 100
+
+  //sensorValLo = (sensorValLo - lo_min)/(61.0)*100.0; //between 101-201
+  val = (sensorValHi - 23)*10 - 5;
   TinyWire.send(val);
+    //val = sensorValLo;
+    //TinyWire.send(val);
 }
 
 void loop() {
-
-  // read in pedal values
-  float sensorValHi = analogRead(inputHi); //read in raw val
-  float sensorValLo = analogRead(inputLo); //read in raw val
-
-  // convert to percents
-  byte percentHi = (sensorValHi - hi_min)/(hi_max - hi_min); //between 0 and 100
-  byte percentLo = (sensorValLo - lo_min)/(lo_max - lo_min) + 101; //between 101-200
-
-  byte val = percentHi;
-  val = percentLo;
-
-  // validate percentages
-  boolean good = valid(percentHi - percentLo);
-  if (!good) {
-      loopCounter++;
-      if(loopCounter == 10) { //it's been false for 90 ms
-        //send error message
-        val = 255; //bad
-      }
-    } else {
-      loopCounter = 0;
-      val = 254; //good
-    }
 }
 
